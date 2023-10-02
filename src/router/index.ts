6f8eff1f08c23c2ddfe5873c319e9d0763f27af8
greenlu2022 <green@ouigifts.com>
2023-10-02 17:54:44 +0800

    feat: added router

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({
+        history: createWebHistory(import.meta.env.BASE_URL),
+        routes: [
+            {
+                path: '/',
+                component: () => import('../layout/default.vue'),
+                children: [
+                    {
+                        path: 'banner',
+                        component: () => import('../pages/Banner.vue'),
+                    },
+                    {
+                        path: 'account',
+                        component: () => import('../pages/Account.vue'),
+                    },
+                    {
+                        path: 'category',
+                        component: () => import('../pages/Category.vue'),
+                    },
+                    {
+                        path: 'coupon',
+                        component: () => import('../pages/Coupon.vue'),
+                    }
+                ],
+            },
+        ]
+    }
+)
+
+export default router
