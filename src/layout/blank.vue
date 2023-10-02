87a5a0c170e64819e4be96f5a6e04d3c3fa24dc5
greenlu2022 <green@ouigifts.com>
2023-10-02 17:54:07 +0800

    feat: added views

---
+++ src/layout/blank.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout-wrapper layout-blank">
+    <RouterView/>
+  </div>
+</template>
+
+<style scoped>
+.layout-wrapper.layout-blank {
+  flex-direction: column;
+}
+</style>
---
+++ src/layout/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+import SideNavigation from "./components/SideNavigation.vue";
+import TopAppBar from "./components/TopAppBar.vue";
+import {RouterView} from "vue-router";
+</script>
+
+<template>
+  <VLayout class="rounded rounded-md">
+    <SideNavigation/>
+    <TopAppBar/>
+    <VMain class="d-flex align-center justify-center" style="min-height: 300px;">
+      <RouterView/>
+    </VMain>
+  </VLayout>
+</template>
+
+<style scoped>
+
+</style>
