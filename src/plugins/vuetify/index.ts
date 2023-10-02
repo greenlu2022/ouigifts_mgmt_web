6c8382c4135b4345dd745237b7e7b7871cf3ea7b
greenlu2022 <green@ouigifts.com>
2023-10-02 17:54:31 +0800

    feat: added theme

---
+++ src/plugins/vuetify/index.ts
@@ -0,0 +1,23 @@
+// Vuetify
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import {aliases, mdi} from 'vuetify/iconsets/mdi'
+import defaults from './default.ts'
+import theme from './theme'
+
+export default createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+            mdi,
+        },
+    },
+    defaults,
+    theme
+})
