56628a7333639224aa91c267f411e82755f1da65
greenlu2022 <green@ouigifts.com>
2023-10-02 17:56:13 +0800

    chore: added eslint router vuetify pinia pkg

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,32 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
+// @ts-ignore
 import App from './App.vue'
+import {createPinia} from 'pinia'
+import vuetify from './plugins/vuetify'
+import router from './router'
 
 
+// // Vuetify
+// import 'vuetify/styles'
+// import {createVuetify} from 'vuetify'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+// import '@mdi/font/css/materialdesignicons.css'
+// import {aliases, mdi} from 'vuetify/iconsets/mdi'
+//
+// const vuetify = createVuetify({
+//     components,
+//     directives,
+//     icons: {
+//         defaultSet: 'mdi',
+//         aliases,
+//         sets: {
+//             mdi,
+//         },
+//     },
+// })
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+const pinia = createPinia()
 
-const vuetify = createVuetify({
-    components,
-    directives,
-    icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-          mdi,
-        },
-      },
-})
-
-createApp(App).use(vuetify).mount('#app')
+createApp(App).use(vuetify).use(pinia).use(router).mount('#app')
