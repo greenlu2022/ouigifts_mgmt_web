06f356520fc67bffe5f05b8121cf6e0ddcf53f93
greenlu2022 <green@ouigifts.com>
2023-11-10 15:30:42 +0800

    feat: implement banner component with banner store

--- src/pages/Banner.vue
+++ src/pages/Banner.vue
@@ -1,9 +1,84 @@
 <script setup lang="ts">
+import {useBannerStore} from "../stores/banner.ts";
+
+const store = useBannerStore()
+store.getBanners()
+
 
 </script>
 
 <template>
-  <h1>Banner</h1>
+  <VTable class="rounded-lg w-100 mt-9" hover fixed-header height="800px">
+    <thead>
+    <tr>
+      <th class="text-uppercase text-center">
+        Image
+      </th>
+      <th class="text-uppercase text-center">
+        Name
+      </th>
+      <th class="text-uppercase text-center">
+        Direct Url
+      </th>
+      <th class="text-uppercase text-center">
+        Action Type
+      </th>
+      <th class="text-uppercase text-center">
+        Start Time
+      </th>
+      <th class="text-uppercase text-center">
+        End Time
+      </th>
+      <th class="text-uppercase text-center">
+        Edit
+      </th>
+    </tr>
+    </thead>
+
+    <tbody>
+    <tr
+        v-for="item in store.banners"
+        :key="item.imageUrl"
+    >
+      <td class="pa-3 w-25">
+        <v-img
+            :width="240"
+            aspect-ratio="16/9"
+            class="ma-auto"
+            cover
+            :src="item.imageUrl"
+        ></v-img>
+      </td>
+      <td class="text-center">
+        {{ item.name }}
+      </td>
+      <td class="text-center">
+        <v-tooltip :text="item.directUrl" location="bottom">
+          <template v-slot:activator="{ props }">
+            <VBtn icon v-bind="props" color="transparent" variant="flat" href="http://google.com">
+              <VIcon color="white">mdi-link-variant</VIcon>
+            </VBtn>
+          </template>
+        </v-tooltip>
+
+      </td>
+      <td class="text-center">
+        {{ item.actionType }}
+      </td>
+      <td class="text-center">
+        {{ item.startTime }}
+      </td>
+      <td class="text-center">
+        {{ item.endTime }}
+      </td>
+      <td class="text-center">
+        <VBtn icon color="transparent" variant="flat">
+          <VIcon color="white">mdi-pencil</VIcon>
+        </VBtn>
+      </td>
+    </tr>
+    </tbody>
+  </VTable>
 </template>
 
 <style scoped>
--- src/stores/banner.ts
+++ src/stores/banner.ts
@@ -1,16 +1,64 @@
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 
 type Banner = {
-    name: string;
+    id: string,
+    name: string,
+    imageUlr: string,
+    directUlr: string,
+    actionType: string,
+    startTime: string,
+    endTime: string
+    isActive: boolean,
 };
 
 export const useBannerStore = defineStore('banner', {
-    state: ():{ banners: any[] } => ({banners: []}),
+    state: (): { banners: any[] } => ({banners: []}),
     getters: {},
     actions: {
-        addBanner(banner: Banner){
+        getBanners(): Banner[] {
+            const apiBanners = [
+                {
+                    id: 1,
+                    name: 'Daoori',
+                    imageUrl: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
+                    directUrl: 6,
+                    actionType: 'inner route',
+                    startTime: '2023-12-31',
+                    endTime: '2024-12-31',
+                    isActive: true,
+                },
+                {
+                    id: 2,
+                    name: 'Apple',
+                    imageUrl: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
+                    directUrl: 6,
+                    actionType: 'inner route',
+                    startTime: '2023-12-31',
+                    endTime: '2024-12-31',
+                    isActive: true,
+                },
+                {
+                    id: 3,
+                    name: 'Google',
+                    imageUrl: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
+                    directUrl: 6,
+                    actionType: 'inner route',
+                    startTime: '2023-12-31',
+                    endTime: '2024-12-31',
+                    isActive: true,
+                }
+            ]
+            if (this.banners.length > 0) {
+                this.clearBanners()
+            }
+            apiBanners.forEach(banner => this.addBanner(banner))
+        },
+        addBanner(banner: Banner) {
             this.banners.push(banner)
+        },
+        clearBanners() {
+            this.banners = []
         }
     },
 })
