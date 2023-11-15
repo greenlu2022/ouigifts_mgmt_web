707bf55b6d9e76f6b860c3d4ea963e7b6582b8d0
greenlu2022 <green@ouigifts.com>
2023-11-15 14:48:55 +0800

    feat: added banner edit modal

---
+++ src/components/BannerEditModal.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+const dialog = ref(false)
+
+</script>
+
+<template>
+  <VRow justify="center">
+    <VDialog
+        v-model="dialog"
+        persistent
+        width="600"
+    >
+      <template v-slot:activator="{ props }">
+        <VBtn icon color="transparent" variant="flat" v-bind="props">
+          <VIcon color="white">mdi-pencil</VIcon>
+        </VBtn>
+      </template>
+      <VCard class="pa-3">
+        <VCardTitle class="text-h5">
+          Banner Setting
+        </VCardTitle>
+        <VRow class="pa-3">
+          <VCol cols="12" sm="12" class="px-3">
+            <VFileInput clearable label="Upload Image"></VFileInput>
+          </VCol>
+          <VCol cols="12" sm="6">
+            <VTextField
+                label="Name"
+                variant="outlined"
+            ></VTextField>
+          </VCol>
+          <VCol cols="12" sm="6">
+            <v-select
+                label="Action Type"
+                :items="['None', 'Inner Route', 'Outer Route']"
+                variant="outlined"
+            ></v-select>
+          </VCol>
+          <VCol cols="12" sm="6">
+            <VTextField
+                placeholder="https://"
+                label="Direct Url"
+                variant="outlined"
+            ></VTextField>
+          </VCol>
+          <VCol cols="12" sm="6">
+            <VTextField
+                placeholder="https://"
+                label="Direct Url"
+                variant="outlined"
+            ></VTextField>
+          </VCol>
+        </VRow>
+        <VCardActions>
+          <VSpacer></VSpacer>
+          <VBtn
+              color="red"
+              variant="text"
+              @click="dialog = false"
+          >
+            Dismiss
+          </VBtn>
+          <VBtn
+              color="green-darken-1"
+              @click="dialog = false"
+          >
+            Save
+          </VBtn>
+        </VCardActions>
+      </VCard>
+    </VDialog>
+  </VRow>
+</template>
+
+<style scoped>
+
+</style>
