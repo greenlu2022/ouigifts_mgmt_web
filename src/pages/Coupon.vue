d51c6312eb540665311b48e582d8c42d3db92afa
greenlu2022 <green@ouigifts.com>
2024-04-16 13:20:57 +0800

    feat: implemented coupon page with data table

--- src/pages/Coupon.vue
+++ src/pages/Coupon.vue
@@ -6,9 +6,19 @@ import {ref} from "vue";
 import CategoryEditDialog from "@/components/CategoryEditDialog.vue";
 import MoreActionMenu from "@/components/MoreActionMenu.vue";
 import DeleteConfirmDialog from "@/components/DeleteConfirmDialog.vue";
+import {useCategoryStore} from "@/stores/category.ts";
 
 const store = useMainCouponStore()
 store.getMainCoupons()
+
+const categoryStore = useCategoryStore()
+categoryStore.getCategories()
+
+const getCategoryNameById = (id: string): string => {
+  const category = categoryStore.categories.find(item => item.id == id)
+  return <string>category?.name
+}
+
 
 const handleEditConfirm = (couponType: CouponType) => {
   store.update(couponType)
@@ -45,11 +55,11 @@ const search = ref<string>("")
 
 <template>
   <VCard class="rounded-lg mt-9">
-    <v-card-title class="d-flex align-center pe-2">
-      <v-icon icon="mdi-cards"></v-icon> &nbsp;
+    <VCardTitle class="d-flex align-center pe-2">
+      <VIcon icon="mdi-cards"></VIcon> &nbsp;
       Main Coupons
-      <v-spacer></v-spacer>
-      <v-text-field
+      <VSpacer></VSpacer>
+      <VTextField
           v-model="search"
           label="Search"
           prepend-inner-icon="mdi-magnify"
@@ -57,10 +67,10 @@ const search = ref<string>("")
           flat
           hide-details
           single-line
-      ></v-text-field>
-    </v-card-title>
-    <v-divider></v-divider>
-    
+      ></VTextField>
+    </VCardTitle>
+    <VDivider></VDivider>
+
     <VDataTable v-model:search="search" :items="store.mainCoupons" :headers="( headers as any)" height="780px"
                 class="rounded-lg"
                 fixed-header>
@@ -78,28 +88,34 @@ const search = ref<string>("")
         </VCard>
       </template>
 
+      <template v-slot:item.categoryIds="{ item }">
+        <VChipGroup>
+          <VChip v-for="id in item.categoryIds ">{{ getCategoryNameById(id) }}</VChip>
+        </VChipGroup>
+      </template>
+
 
       <template v-slot:item.isLimited="{ item }">
         <div class="text-end">
-          <v-chip
+          <VChip
               :color="item.isLimited ? 'red' : 'green'"
               :text="item.isLimited ? 'Limited' : 'Infinite'"
               class="text-uppercase"
               size="small"
               label
-          ></v-chip>
+          ></VChip>
         </div>
       </template>
 
       <template v-slot:item.isEnabled="{ item }">
         <div class="text-end">
-          <v-chip
+          <VChip
               :color="item.isEnabled ? 'green' : 'red'"
               :text="item.isEnabled ? 'activated' : 'disabled'"
               class="text-uppercase"
               size="small"
               label
-          ></v-chip>
+          ></VChip>
         </div>
       </template>
 
@@ -115,118 +131,6 @@ const search = ref<string>("")
       </template>
     </VDataTable>
   </VCard>
-
-
-  <!--  <VTable class="rounded-lg w-100 mt-9" fixed-header height="800px">-->
-  <!--    <thead>-->
-  <!--    <tr>-->
-  <!--      <th class="text-uppercase text-center">-->
-  <!--        Image-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center">-->
-  <!--        Category-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center">-->
-  <!--        Name-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        Code-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        discountType-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        discountPercentage-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        isLimited-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        availableAmount-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        restriction-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        startTime-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        endTime-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        Active-->
-  <!--      </th>-->
-  <!--      <th class="text-uppercase text-center w140">-->
-  <!--        More-->
-  <!--      </th>-->
-  <!--    </tr>-->
-  <!--    </thead>-->
-
-  <!--    <tbody>-->
-  <!--    <tr-->
-  <!--        v-for="item in store.mainCoupons"-->
-  <!--        :key="item.id"-->
-  <!--    >-->
-  <!--      <td class="pa-3 w-25">-->
-  <!--        <VImg-->
-  <!--            :width="240"-->
-  <!--            aspect-ratio="16/9"-->
-  <!--            class="ma-auto"-->
-  <!--            cover-->
-  <!--            :src="item.imageUrl"-->
-  <!--        ></VImg>-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.categoryIds }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.name }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.code }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.discountType }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.discountPercentage }}-->
-  <!--      </td>-->
-  <!--      <td class="center">-->
-  <!--        <VCheckbox-->
-  <!--            v-model="item.isLimited"-->
-  <!--            @change="handleEditConfirm(item)"-->
-  <!--            hide-details-->
-  <!--        ></VCheckbox>-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.availableAmount }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.restriction }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.startTime }}-->
-  <!--      </td>-->
-  <!--      <td class="text-center">-->
-  <!--        {{ item.endTime }}-->
-  <!--      </td>-->
-
-  <!--      <td class="center">-->
-  <!--        <VSwitch class="justify-center" v-model="item.isEnabled" @change="handleEditConfirm(item)"></VSwitch>-->
-  <!--      </td>-->
-  <!--      <td>-->
-  <!--        <MoreActionMenu>-->
-  <!--          <template #edit>-->
-  <!--            <CategoryEditDialog :category="item" @@confirm="handleEditConfirm"></CategoryEditDialog>-->
-  <!--          </template>-->
-  <!--          <template #delete>-->
-  <!--            <DeleteConfirmDialog @@delete="handleDeleteConfirm(item.id!!)"></DeleteConfirmDialog>-->
-  <!--          </template>-->
-  <!--        </MoreActionMenu>-->
-  <!--      </td>-->
-  <!--    </tr>-->
-  <!--    </tbody>-->
-  <!--  </VTable>-->
 </template>
 
 <style scoped>
--- src/stores/mainCoupon.ts
+++ src/stores/mainCoupon.ts
@@ -10,7 +10,7 @@ export const useMainCouponStore = defineStore('mainCoupon', {
         getMainCoupons() {
             const apiMainCoupons: CouponType[] = [{
                 id: 1,
-                categoryIds: ["1"],
+                categoryIds: ["1", "2"],
                 name: "Apple",
                 code: "APPLE_202403",
                 isLimited: true,
