cf08b2c30bb0358edd308cc211029f393a6fe729
greenlu2022 <green@ouigifts.com>
2024-05-13 16:40:07 +0800

    chore: added type/vuelidate package

--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,8 @@ import {createPinia} from 'pinia'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
-
-// // Vuetify
-// import 'vuetify/styles'
-// import {createVuetify} from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
-// import '@mdi/font/css/materialdesignicons.css'
-// import {aliases, mdi} from 'vuetify/iconsets/mdi'
-//
-// const vuetify = createVuetify({
-//     components,
-//     directives,
-//     icons: {
-//         defaultSet: 'mdi',
-//         aliases,
-//         sets: {
-//             mdi,
-//         },
-//     },
-// })
+import Vuelidate from '@vuelidate/core'
 
 const pinia = createPinia()
 
-createApp(App).use(vuetify).use(pinia).use(router).mount('#app')
+createApp(App).use(vuetify).use(pinia).use(router).use(Vuelidate).mount('#app')
