a5b9ab940393775b6d610412572ecae8199bf9e9
greenlu2022 <green@ouigifts.com>
2024-05-31 17:21:32 +0800

    feat: form validation

--- src/components/BannerEditDialog.vue
+++ src/components/BannerEditDialog.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import {reactive, ref} from 'vue'
 import DateTimePickField from "./DateTimePickField.vue"
 import type Banner from "@/types/Banner.ts"
+import {required, url} from "@vuelidate/validators";
+import {endDateAfterStartDate, isUrlWhenActionTypeIsNotNone} from "@/validators/formValidators.ts";
+import {useVuelidate} from "@vuelidate/core";
 
 
 const outerProps = defineProps<{
@@ -16,12 +19,49 @@ const emit = defineEmits<{
 const dialog = ref(false)
 const refInputEl = ref<HTMLElement>()
 
-const dataLocal = ref<Banner>({...outerProps.banner})
 
-const handleConfirm = () => {
-  emit("@confirm", dataLocal.value)
-  dialog.value = false
-}
+const localFormData = reactive<Banner>({
+  id: outerProps.banner.id ? outerProps.banner.id : 0,
+  name: outerProps.banner.name ? outerProps.banner.name : "",
+  imageUrl: outerProps.banner.imageUrl ? outerProps.banner.imageUrl : "https://cdn.vuetifyjs.com/images/parallax/material.jpg",
+  directUrl: outerProps.banner.directUrl ? outerProps.banner.directUrl : "",
+  actionType: outerProps.banner.actionType ? outerProps.banner.actionType : "None",
+  startTime: outerProps.banner.startTime ? outerProps.banner.startTime : "",
+  endTime: outerProps.banner.endTime ? outerProps.banner.endTime : "",
+  isEnabled: outerProps.banner.isEnabled ? outerProps.banner.isEnabled : false,
+})
+
+const rules = {
+  imageUrl: {
+    required,
+    url
+  },
+  name: {
+    required
+  },
+  directUrl: {
+    isUrlWhenActionTypeIsNotNone
+  },
+  actionType: {
+    required
+  },
+  endTime: {
+    endDateAfterStartDate
+  }
+};
+
+const v$ = useVuelidate(rules, localFormData);
+
+const handleSubmit = async () => {
+  const isFormCorrect = await v$.value.$validate();
+  if (isFormCorrect) {
+    emit("@confirm", {...localFormData});
+    dialog.value = false;
+    v$.value.$reset();
+  } else {
+    return
+  }
+};
 
 
 // changeAvatar function
@@ -33,7 +73,7 @@ const changeAvatar = (file: Event) => {
     fileReader.readAsDataURL(files[0])
     fileReader.onload = () => {
       if (typeof fileReader.result === 'string') {
-        dataLocal.value.imageUrl = fileReader.result
+        Object.assign(localFormData, {imageUrl: fileReader.result})
       }
     }
   }
@@ -62,7 +102,7 @@ const changeAvatar = (file: Event) => {
           <VAvatar
               rounded="lg"
               size="120"
-              :image="dataLocal.imageUrl"
+              :image="localFormData.imageUrl"
           />
         </VCol>
         <VCol cols="12" sm="9" class="px-3">
@@ -89,7 +129,7 @@ const changeAvatar = (file: Event) => {
           <VTextField
               label="Name"
               variant="outlined"
-              v-model="dataLocal.name"
+              v-model="localFormData.name"
           ></VTextField>
         </VCol>
         <VCol cols="12" sm="6">
@@ -97,7 +137,7 @@ const changeAvatar = (file: Event) => {
               label="Action Type"
               :items="['None', 'Inner Route', 'Outer Route']"
               variant="outlined"
-              v-model="dataLocal.actionType"
+              v-model="localFormData.actionType"
           ></VSelect>
         </VCol>
         <VCol cols="12" sm="6">
@@ -105,14 +145,19 @@ const changeAvatar = (file: Event) => {
               placeholder="https://"
               label="Direct Url"
               variant="outlined"
-              v-model="dataLocal.directUrl"
+              v-model="localFormData.directUrl"
+              :disabled="localFormData.actionType === 'None'"
           ></VTextField>
         </VCol>
         <VCol cols="12" sm="6">
-          <DateTimePickField label="Start Date" v-model="dataLocal.startTime"></DateTimePickField>
+          <DateTimePickField label="Start Date" v-model="localFormData.startTime"></DateTimePickField>
         </VCol>
         <VCol cols="12" sm="6">
-          <DateTimePickField label="End Date" v-model="dataLocal.endTime"></DateTimePickField>
+          <DateTimePickField
+              label="End Date"
+              v-model="localFormData.endTime"
+              :error-messages="v$.endTime.$errors.map((e: any):string => e.$message)"
+          ></DateTimePickField>
         </VCol>
       </VRow>
       <VCardActions>
@@ -126,7 +171,7 @@ const changeAvatar = (file: Event) => {
         </VBtn>
         <VBtn
             color="green-darken-1"
-            @click="handleConfirm"
+            @click="handleSubmit"
         >
           Save
         </VBtn>
