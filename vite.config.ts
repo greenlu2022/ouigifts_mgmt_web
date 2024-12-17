76cd8fba44fc672f36f041d692de75db7d685649
greenlu2022 <green@ouigifts.com>
2024-12-17 18:42:11 +0800

    chore: add vercel package

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import vercel from 'vite-plugin-vercel';
 import {fileURLToPath, URL} from 'node:url'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), vuetify()],
+    plugins: [vue(), vuetify(), vercel()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
